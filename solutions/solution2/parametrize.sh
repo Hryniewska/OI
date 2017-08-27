5efeca7a7acafd307151a1b3174e6d1729edbd3e
Hryniewska <werhry@gmail.com>
2017-08-27 23:04:17 +0200

    Delete parametrize.sh

--- solutions/solution2/parametrize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ZMIENNA=10
-while [ $ZMIENNA -le 90 ]; do
-./bin/run --param $ZMIENNA --solution ../../environment/data/test/images/
-cp solution_result.txt wynik_$ZMIENNA.txt
-ZMIENNA=$[$ZMIENNA + 10]
-done
